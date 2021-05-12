bdc3d11bbacdf02dc29a7bb650797a2d457559b0
Yashvardhan Jain <yashvardhan18@yahoo.com>
2021-05-12 07:53:15 +0530

    Minor Fix

--- propre_html_mail.py
+++ propre_html_mail.py
@@ -169,25 +169,25 @@ def send_email(strFrom, strTo, PASSWORD, data):
 
     msgAlternative.attach(msgText)
 
-    fp = open(r'images\logo.png', 'rb')
+    fp = open('images\logo.png', 'rb')
     msgImage = MIMEImage(fp.read())
     fp.close()
     msgImage.add_header('Content-ID', '<logo>')
     msgRoot.attach(msgImage)
 
-    fp = open(r'images\github.png', 'rb')
+    fp = open('images\github.png', 'rb')
     msgImage = MIMEImage(fp.read())
     fp.close()
     msgImage.add_header('Content-ID', '<github>')
     msgRoot.attach(msgImage)
 
-    fp = open(r'images\email.png', 'rb')
+    fp = open('images\email.png', 'rb')
     msgImage = MIMEImage(fp.read())
     fp.close()
     msgImage.add_header('Content-ID', '<email>')
     msgRoot.attach(msgImage)
 
-    fp = open(r'images\linkedin.png', 'rb')
+    fp = open('images\linkedin.png', 'rb')
     msgImage = MIMEImage(fp.read())
     fp.close()
     msgImage.add_header('Content-ID', '<linkedin>')
